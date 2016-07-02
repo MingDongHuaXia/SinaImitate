8ee9d6e4b9d415a48c416c9f351785b2f95945be
明动华夏 <524981259@qq.com>
2016-07-02 22:38:04 +0800

    项目准备

---
+++ SinaImiateOC/SinaImiateOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SinaImiateOC
+//
+//  Created by xiaoming on 16/7/2.
+//  Copyright © 2016年 GoodJob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SinaImiateOC/SinaImiateOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SinaImiateOC
+//
+//  Created by xiaoming on 16/7/2.
+//  Copyright © 2016年 GoodJob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
